e99628dd3c57641d8e8a7e3e82e8ff72c67181ca
youngseok <dblys152@gmail.com>
2023-06-03 17:49:50 +0900

    gateway 추가

--- a_service/src/main/java/com/ys/aservice/in/AServiceController.java
+++ a_service/src/main/java/com/ys/aservice/in/AServiceController.java
@@ -3,9 +3,11 @@ package com.ys.aservice.in;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/a-service")
 public class AServiceController {
 
     @GetMapping("/test")
---
+++ gateway/src/main/java/com/ys/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ys.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.ys.gateway")
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
