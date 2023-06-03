f0c10383cd7921462ed01f4e782db0fc8e9acab8
youngseok <dblys152@gmail.com>
2023-06-03 17:38:29 +0900

    eureka client b server 추가

---
+++ b_service/src/main/java/com/ys/bservice/in/BServiceController.java
@@ -0,0 +1,15 @@
+package com.ys.bservice.in;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BServiceController {
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity.status(HttpStatus.OK).body("I'm B Service!");
+    }
+}
