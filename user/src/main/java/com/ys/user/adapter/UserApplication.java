5ed559fa56397468d69e3b3e4da387b42f0b28cd
youngseok <dblys152@gmail.com>
2023-12-11 23:41:27 +0900

    서비스 모듈 재조정

--- user/src/main/java/com/ys/user/adapter/UserApplication.java
+++ user/src/main/java/com/ys/user/adapter/UserApplication.java
@@ -1,25 +1,22 @@
-package com.ys.gateway;
+package com.ys.user.adapter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@ComponentScan(basePackages = "com.ys.gateway")
+@ComponentScan(basePackages = "com.ys.user")
 @RestController
-public class GatewayApplication {
-
-	@GetMapping("/")
-	public String test() {
-		return "hello";
-	}
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
 }
