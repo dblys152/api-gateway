f4f37ec0df5579c19b90496c966489b435f8f1c3
youngseok <dblys152@gmail.com>
2024-03-05 23:58:30 +0900

    gateway redis rate limiter 설정 추가

--- gateway/src/main/java/com/ys/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/ys/gateway/config/GatewayConfig.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ys.gateway.filter.TokenHeaderGlobalFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -23,13 +26,26 @@ public class GatewayConfig {
                         .path("/api/sign-up/**",
                                 "/api/users/**")
                         .uri("lb://USER-SERVICE"))
+                .route("authentication-service-limit", route -> route
+                        .path("/api/login/**")
+                        .filters(f -> f.requestRateLimiter(r -> r.setRateLimiter(redisRateLimiter())))
+                        .uri("lb://AUTHENTICATION-SERVICE"))
                 .route("authentication-service", route -> route
-                        .path("/api/login/**",
-                                "/api/token/**",
+                        .path("/api/token/**",
                                 "/api/token-info/**",
                                 "/api/logout/**")
                         .uri("lb://AUTHENTICATION-SERVICE"))
                 .build();
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(20, 40);
+    }
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 
     @Bean
